4ad9b78403c514f7917c8e8f189228acb8771572
taeuk-gang <gmdkrl@hanyang.ac.kr>
2019-07-01 13:13:35 +0900

    :new: Storybook.js를 이용한 UI 테스트 환경 구축

---
+++ src/stories/index.stories.js
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/polymer'
+// import { document } from 'global'
+import '../components/App.js'
+  
+storiesOf(`Exam`, module)
+	.add(`exam-element`, () => `<exam-element></exam-element>`)
