ac19b5f427ca885add01145a3ba80bbe70454498
taeuk-gang <gmdkrl@hanyang.ac.kr>
2019-07-17 16:40:50 +0900

    :wrench: Git page 미사용으로 인한 bundle파일 gitignore 처리

--- src/main.js
+++ src/main.js
@@ -1,29 +1,15 @@
-<<<<<<< HEAD
 import './components/page-main.js'
-=======
-import './components/App.js'
-import './components/Login.js'
->>>>>>> f59ad0b49c8dd1970508b2f41ea8d11720885eb6
 
 export class Main {
 	static init() {
 		this.loadingDOM()
 
-<<<<<<< HEAD
-=======
-		// PJAX 방식
-		// this._onPopstate()
-		// this._onClickAnchor()
-		// this.router(`/`)
-
->>>>>>> f59ad0b49c8dd1970508b2f41ea8d11720885eb6
 		this.router()
 			.isRoute()
 			.otherwise()
 			.pushUrl()
 	}
 
-<<<<<<< HEAD
 	static router() {
 		let path = location.pathname
 		let isContinue = true
@@ -32,70 +18,14 @@ export class Main {
 			isRoute() {
 				if (isContinue) {
 					Main.renderPage(`page-${path.split(`/`)[1]}`, path)
-=======
-	static _onPopstate() {
-		window.addEventListener(`popstate`, event => {
-			console.info(`[popstate]`, event.state)
-			this.router(event.state.path)
-		})
-	}
-
-	static _onClickAnchor() {
-		document.addEventListener(`click`, event => {
-			if (!event.target || event.target.nodeName !== `A`) {
-				return
-			}
-			event.preventDefault()
-		
-			const path = event.target.getAttribute(`href`)
-		
-			history.pushState({ path }, null, path)
-		
-			this.router(path)
-		})
-	}
-
-	// PJAX 방식
-	// static get routes() {
-	// 	return {
-	// 		'/': function () {
-	// 			Main.renderPage(`app-main`, `/`)
-	// 		},
-	// 		'/login': function () {
-	// 			Main.renderPage(`login-main`, `/login`)
-	// 		},
-	// 		otherwise() {
-	// 			Main.exceptDOM()
-	// 		},
-	// 	}
-	// }
-
-	// static router(path) {
-	// 	(this.routes[path] || this.routes.otherwise)(path)
-	// }
-
-	static router() {
-		let path = location.pathname
-		let isContinue = true
-		if (path === `/`) {path = `/app`}		
-		return {
-			isRoute() {
-				if (isContinue) {
-					Main.renderPage(`${path.split(`/`)[1]}-main`, path)
->>>>>>> f59ad0b49c8dd1970508b2f41ea8d11720885eb6
 					isContinue = false
 				}
 				return this
 			},
 			otherwise() {
 				if (isContinue) {
-<<<<<<< HEAD
 					Main._onLoad(() => history.replaceState({}, null, `/main`))
 					Main.renderPage(`page-main`, path)
-=======
-					Main._onLoad(() => history.replaceState({}, null, `login`))
-					Main.renderPage(`login-main`, path)
->>>>>>> f59ad0b49c8dd1970508b2f41ea8d11720885eb6
 					isContinue = false
 				}
 				return this
--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,21 +1,11 @@
 import { storiesOf } from '@storybook/polymer'
 // import { document } from 'global'
-<<<<<<< HEAD
 import '../components/page-main.js'
 
 storiesOf(`Page`, module)
 	.add(`page-main`, () => `<page-main></page-main>`)
-=======
-import '../components/App.js'
-import '../components/Login.js'
 
-storiesOf(`App`, module)
-	.add(`app-main`, () => `<app-main></app-main>`)
-	.add(`login-main`, () => `<login-main></login-main>`)
-	.add(`login-form`, () => `<login-form></login-form>`)
->>>>>>> f59ad0b49c8dd1970508b2f41ea8d11720885eb6
-
-// storiesOf(`Demo`, module)
+// storiesOf(`Demo`, module)W
 // 	.add(`heading`, () => `<h1>Hello World</h1>`)
 // 	.add(`button`, () => {
 // 		const button = document.createElement(`button`)
