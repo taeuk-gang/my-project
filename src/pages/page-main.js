24351a09f90654abcb10c05373723ffcea06f964
taeuk-gang <gmdkrl@hanyang.ac.kr>
2019-09-10 10:18:10 +0900

    :art: Page Css 적용

--- src/pages/page-main.js
+++ src/pages/page-main.js
@@ -5,25 +5,35 @@ import i18next from 'i18next'
 export class PageMain extends LitElement {
 	static get styles() {        
 		return css`
-		
+		:host {
+			display: flex;
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100vw;
+			height: 100vh;
+			margin: 0;
+			padding: 0;
+			justify-content: center;
+			align-items: center;
+		}
 		`
 	}
 
 	static get properties() {
 		return {
-			prop1: String,
+			title: String,
 		}
 	}
 	
 	constructor() {
 		super()
+		this.title = i18next.t(`APP_NAME`)
 	}
 
 	render() {
-		return html`		
-		<div id="pageMain">			
-			${i18next.t(`APP_NAME`)}
-        </div>
+		return html`
+		${this.title}
         `
 	}
 }
